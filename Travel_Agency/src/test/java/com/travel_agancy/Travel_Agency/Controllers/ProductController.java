5137cef776e88daed681a868c04e9bd557265ea4
vasalerm <vasalerm008@gmail.com>
2023-05-24 23:14:48 +0300

    Travel Agency

---
+++ Travel_Agency/src/test/java/com/travel_agancy/Travel_Agency/Controllers/ProductController.java
@@ -0,0 +1,11 @@
+package com.travel_agancy.Travel_Agency.Controllers;
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class ProductController {
+    public String Products(){
+        return "products";
+    }
+}
---
+++ Travel_Agency/src/test/java/com/travel_agancy/Travel_Agency/TravelAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel_agancy.Travel_Agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
